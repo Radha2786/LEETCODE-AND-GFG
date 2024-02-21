f4ecd834072a59ebb654e8c71c045710936db1a9
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-02-22 00:00:38 +0530

    Time: 55 ms (33.84%) | Memory: 44.2 MB (83.52%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,16 +1,15 @@
+
 class Solution {
     public int longestStrChain(String[] words) {
-        List<String> arr = Arrays.asList(words);
-
        
-        arr.sort(comp);
-        int n = arr.size();
+        Arrays.sort(words,comp);
+        int n = words.length;
         int max = 1;
         int[] dp = new int[n];
         Arrays.fill(dp,1);
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(Compare(arr.get(i),arr.get(j))){
+                if(Compare(words[i],words[j])){
                     dp[i]= Math.max(dp[j]+1,dp[i]);
                     max = Math.max(dp[i],max);
                 }
