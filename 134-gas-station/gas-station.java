8ef157f50861e8443732112e757f8607d1c67768
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-03-03 15:00:32 +0530

    Time: 2 ms (99.43%) | Memory: 56.6 MB (83.61%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,10 +1,6 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int s1=sum(gas);
-        int s2=sum(cost);
-        if(s1<s2){
-            return -1;
-        }
+       int totalleft=0;
         int remainingfuel=0, index=0;
         for(int i=0;i<gas.length ; i++){
             remainingfuel+=gas[i]-cost[i];
@@ -12,14 +8,9 @@ class Solution {
                 index=i+1;
                 remainingfuel=0;
             }
+            totalleft+=gas[i]-cost[i];
         }
-        return index;
+        return totalleft<0?-1:index;
     }
-    public static int sum(int[] arr){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-        return sum;
-    }
+  
 }
