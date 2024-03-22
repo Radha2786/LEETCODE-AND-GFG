fe57cfbbafdc98f61e3c0e3a3a2de3fc5b3d9454
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-03-23 02:01:30 +0530

    Time: 7 ms (98.28%) | Memory: 56.8 MB (57.42%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ll = new ArrayList<>();
-        List<String> ls = new ArrayList<>();
-        return Palindrome_Partioning(s, ls,ll);
+        List<String> ll = new ArrayList<>();
+        List<List<String>> ls = new ArrayList<>();
+        Partitioning(s,ll,ls);
+        return ls; 
+    }
+    public static void Partitioning(String question,List<String> list,  List<List<String>> ll){
+
+        if(question.isEmpty()){
+            ll.add(new ArrayList<>(list));
+            return ;
+        }
+        
+        for(int i=1; i<=question.length();i++){
+            String str = question.substring(0,i);
+            if(isPallindrome(str)){
+                list.add(str);
+                Partitioning(question.substring(i),list,ll);
+                list.remove(list.size()-1);
+            }
+        }
+       
     }
 
-     public List<List<String>> Palindrome_Partioning(String str, List<String> ls, List<List<String>> ll){
-          if(str.isEmpty()){
-              ll.add(new ArrayList<>(ls));
-          }
-
-          for(int i=1;i<=str.length() ;i++){
-             String p = str.substring(0,i);
-             if(isPalindrome(p)){
-                 ls.add(p);
-                  Palindrome_Partioning(str.substring(i),ls,ll);
-              ls.remove(ls.size()-1);
-             }
-            
-            
-          }
-          return ll;
-          
-      }
-      static boolean isPalindrome(String st){
-        int s = 0;
-        int e = st.length()-1;
-
-        while (s < e){
-            if(st.charAt(s) != st.charAt(e)){
+    public static boolean isPallindrome(String str){
+        int s=0;
+        int e= str.length()-1;
+        while(s<e){
+            if(str.charAt(s)!=str.charAt(e)){
                 return false;
             }
             s++;
