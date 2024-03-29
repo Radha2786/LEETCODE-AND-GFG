b287690d1eaf0e2889711c3299e47817bd117842
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-03-29 10:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (83.81%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    // floyd cycle detection algorithm
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while( fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
