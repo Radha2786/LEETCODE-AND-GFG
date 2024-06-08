ddf302f6a4781ba387997cd730b9da79aafcdcf2
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-06-08 17:26:14 +0530

    Time: 0 ms (100%), Space: 42 MB (16.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,25 @@
 class Solution {
+    // Dutch National flag Algorithm (Striver-- intution)
     public void sortColors(int[] nums) {
-        int n = nums.length ;
-        for(int i=0;i<n; i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    int temp = nums[i];
-                    nums[i]=nums[j];
-                    nums[j]= temp;
-                }
+        int low = 0 , mid = 0 ;
+        int high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                // swap arr[0] and arr[mid] and increase both
+                int temp = nums[mid];
+                nums[mid]=nums[low];
+                nums[low]= temp;
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                // swap arr[mid] and arr[high]
+                int temp = nums[mid];
+                nums[mid]=nums[high];
+                nums[high]= temp;
+                high--;
             }
-        }
+        } 
     }
-    
 }
