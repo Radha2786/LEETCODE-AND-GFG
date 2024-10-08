2ddce02916de264fb8a4f5a44750015a7968abf9
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-10-09 02:28:16 +0530

    Time: 2 ms (62.73%), Space: 45.9 MB (70.29%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,59 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        int p1 = 0, p2 = 0, i=0;
-        int[] ans = new int[nums1.length + nums2.length];
-        // step 1 Merge Two sorted Arrays
-        while(p1 < nums1.length && p2 < nums2.length){
-            if(nums1[p1] <= nums2[p2]){
-                ans[i] = nums1[p1];
+   public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Optimised approach (reduced space of extra array)
+        int n = nums1.length + nums2.length;
+        int index2 = n/2;
+        int index1 = n/2 - 1;
+        int p1 = 0, p2=0, count =0;
+        int ele1 = -1, ele2 =-1;
+     
+        while(p1<nums1.length && p2<nums2.length){
+            if(nums1[p1]<nums2[p2]){
+                if(count == index1){
+                    ele1 = nums1[p1];
+                }
+                if(count == index2){
+                    ele2 = nums1[p1];
+                }
                 p1++;
+                count++;
             }else{
-                ans[i] = nums2[p2];
+                if(count == index1){
+                    ele1 = nums2[p2];
+                }
+                if(count == index2){
+                    ele2 = nums2[p2];
+                }
                 p2++;
+                count++;
             }
-            i++;
         }
         while(p1<nums1.length){
-            ans[i]=nums1[p1];
+            if(count == index1){
+                ele1 = nums1[p1];
+            }
+            if(count == index2){
+                ele2 = nums1[p1];
+            }
             p1++;
-            i++;
+            count++;
         }
         while(p2<nums2.length){
-            ans[i]=nums2[p2];
+            if(count == index1){
+                ele1 = nums2[p2];
+            }
+            if(count == index2){
+                ele2 = nums2[p2];
+            }
             p2++;
-            i++;
+            count++;
         }
-        for(int k=0;k<ans.length;k++){
-            System.out.println(ans[k]);
+    
+
+        if(n%2 == 0){
+            return (double)((double)(ele1+ele2))/2;
         }
-        // check if length is even or odd of newly created array 
-        int n = ans.length;
-        if(n % 2 ==0){
-            int firstVal = ans[n/2];
-            int secondVal = ans[(n/2) -1];
-             System.out.println(secondVal);
-             System.out.println(firstVal);
-            double val = ( (double) firstVal + (double) secondVal)/2;
-            System.out.println(val);
-            return val;
-        }else{
-           
-             return ans[n/2];
-        }
+        return ele2;
+
     }
 }
