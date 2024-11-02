1f2559b6800bcd9c986de9b1fca19861277c0295
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-11-03 01:25:34 +0530

    Time: 5 ms (8.93%) | Memory: 42.4 MB (51.48%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,36 +1,41 @@
 class Solution {
-    public int myAtoi(String str) {
-        int ans = 0;
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
-        int sign = 1 ; //change the sign to -1 if negative 
-        int size= str.length();
-        int i = 0;
-        if(size==0){
+    public int myAtoi(String s) {
+        if(s.length()==0){
             return 0;
         }
-        while(i < size && str.charAt(i)==' '){
-            i++;
-        }
-        System.out.println(str);
-        System.out.println(size);
-        if(i < size && (str.charAt(i)=='-' || str.charAt(i)=='+')){
-            if(str.charAt(i)=='-'){
-                sign = -1;
-            }
+
+        int i=0;
+        int sign =1;
+
+        while(i<s.length() && s.charAt(i)==' '){
             i++;
         }
 
-        while(i<size && str.charAt(i)-'0'<=9 && str.charAt(i)-'0'>=0){
-            int digit = str.charAt(i)-'0';
-            if(ans > max/10 || ans == max/10 && digit>max%10 ){
-              return  (sign == 1) ? max : min ;
-            }
-            ans= ans*10+digit;
+        if(i<s.length() && s.charAt(i)=='-'){
+            sign = -1;
+            i++;
+        }else if(i<s.length() && s.charAt(i)=='+'){
             i++;
         }
-        return sign*ans;
+        System.out.println(i);
 
-
+        long num=0;
+        while(i<s.length() && s.charAt(i) - '0' <=9 && s.charAt(i) - '0' >=0){
+            int digit = s.charAt(i)- '0';
+            num = num*10+digit;
+            if(sign==-1){
+                long check = -num;
+                if(check < Integer.MIN_VALUE){
+                    return Integer.MIN_VALUE;
+                    }
+            }else{
+                if(num > Integer.MAX_VALUE){
+                        return Integer.MAX_VALUE;
+                }
+                }
+                i++;
+        } 
+                            
+        return (int) num*sign;
     }
 }
