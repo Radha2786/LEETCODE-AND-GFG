635fe60e622a1db997697adb4889208583a1d30f
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2024-11-04 02:58:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,27 @@
 class Solution {
     public double myPow(double x, int n) {
-        long nn = n;
-        if(nn<0){
-            nn = -1 * nn;
-        }
-        Double ans = Power(x,nn);
         if(n<0){
-            return 1/ans;
+          n = -1*n;
+           Double ans = calcPower(x,n);
+           return 1/ans;
         }else{
-            return ans;
+            Double ans = calcPower(x,n);
+        return ans;
         }
+        
     }
-    public static double Power(double x , long nn){
-        if(nn==0){
+
+    public static double calcPower(double x, int n){
+
+        if(n==0){
             return 1;
         }
 
-        if(nn % 2 == 0){
-           return Power(x*x,nn/2);
+        if(n % 2 == 0){
+            return calcPower(x*x,n/2);
         }else{
-            return x*Power(x,nn-1);
+           return x*calcPower(x,n-1);
         }
-
+       
     }
 }
