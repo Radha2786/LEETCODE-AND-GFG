203c84a24ebc0ed9b079f0a96e80c6b54a3bcbad
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2025-02-02 03:08:30 +0530

    Time: 1 ms (37.43%), Space: 43.5 MB (32.64%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -5,9 +5,7 @@ class Solution {
         }
         int i=0;
         while(i<nums.length-1){
-            if(nums[i]%2==0 && nums[i+1]%2 ==0){
-                return false;
-            }else if(nums[i]%2!=0 && nums[i+1]%2 !=0){
+            if(nums[i]%2==0 && nums[i+1]%2 ==0 || nums[i]%2!=0 && nums[i+1]%2 !=0){
                 return false;
             }
             i++;
