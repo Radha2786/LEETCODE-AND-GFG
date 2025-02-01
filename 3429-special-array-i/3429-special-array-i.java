80e71d0370ade041e5fc5922e520c59bab611068
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2025-02-02 03:05:13 +0530

    Time: 0 ms (100%), Space: 43.4 MB (56.55%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==1){
+            return true;
+        }
+        int i=0;
+        while(i<nums.length-1){
+            if(nums[i]%2==0 && nums[i+1]%2 ==0){
+                return false;
+            }else if(nums[i]%2!=0 && nums[i+1]%2 !=0){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
