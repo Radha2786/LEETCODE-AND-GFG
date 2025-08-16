cc3b3812697241eb0482c4079d0a0d19b7826d7d
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2025-08-16 16:28:56 +0530

    Time: 2 ms (83.17%), Space: 44.6 MB (81.61%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> list = new ArrayList<>();
         List<List<Integer>> ll = new ArrayList<>();
-        Combination(candidates, target, list, ll, 0,0);
+        List<Integer> list = new ArrayList<>();
+        Combination(candidates, target, ll, list, 0);
         return ll;
     }
-    public static void Combination(int[] arr, int target, List<Integer> list, List<List<Integer>> ll, int sum, int index){
-        if(sum==target){
+
+    public static void Combination(int[] candidates, int target,List<List<Integer>> ll, List<Integer> list, int index){
+        if(target==0){
             ll.add(new ArrayList<>(list));
-            // ll.add(list);
             return;
         }
-        if(sum>target){
-            return;
-        }
-        for(int i=index;i<arr.length;i++){
-            list.add(arr[i]);
-            sum+=arr[i];
-            Combination(arr, target, list, ll, sum,i);
-            list.remove(list.size()-1);
-            sum=sum-arr[i];
+        for(int i=index;i<candidates.length;i++){
+            if(target>=candidates[i]){
+                list.add(candidates[i]);
+                Combination(candidates, target-candidates[i], ll, list, i);
+                list.remove(list.size()-1);
+            }  
+
         }
     }
 }
