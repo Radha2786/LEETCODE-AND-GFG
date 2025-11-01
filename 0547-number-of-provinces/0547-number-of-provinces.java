6c04b74650813d4b8e30eea92efd108bb178687e
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2025-11-02 01:30:31 +0530

    Time: 12 ms (5.05%), Space: 48.5 MB (9.52%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,63 +1,51 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-
-        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();;
-
-        Graph1(isConnected.length,map);
-
-         for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected[i].length;j++){
-                 if (isConnected[i][j] == 1 && i != j) {
-            AddEdge(i + 1, j + 1, 0, map);
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<isConnected.length;i++){
+            adj.add(new ArrayList<>());
         }
-            }
-        }
-
-       return BFT(map);
-    }
-
-    public void Graph1(int v,HashMap<Integer, HashMap<Integer, Integer>> map){
         
-        for(int i=1;i<=v;i++){
-            map.put(i,new HashMap<Integer, Integer>());
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[i].length;j++){
+                if(isConnected[i][j]!=0 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
         }
-    }
-     public void AddEdge(int v1, int v2, int cost,HashMap<Integer, HashMap<Integer, Integer>> map){
-        map.get(v1).put(v2,cost); // v1--> map get kra then put v2 and Cost
-        map.get(v2).put(v1,cost);
+         int ans = DFT(adj);
+         return ans;
     }
 
-     public int BFT(HashMap<Integer, HashMap<Integer, Integer>> map){
-        int count=0;
-        Queue<Integer> queue = new LinkedList<>();
-        HashSet<Integer> visited = new HashSet<>();
-        for(int key: map.keySet()){
-            if(visited.contains(key)){
-                continue;
-            }
-            count++; // in order to calculate no. of components or parts for disconnected graph
-            queue.add(key);
-            while(!queue.isEmpty()){
-                // 1. remove
-                int nbr=queue.poll();
-                // 2. ignore if already visited
-                if(visited.contains(nbr)){
-                    continue;
-                }
-                //3. add visited
-                visited.add(nbr);
-                //4. selfwork
-                // System.out.print(nbr+" ");
-                //5. add unvisited neighbours
-                for(int nbrs: map.get(nbr).keySet()){
-                    if(!visited.contains(nbrs)){
-                        queue.add(nbrs);
-                    }
-                }
-            }
+    public int DFT(List<List<Integer>> adj){
+         int count = 0;
+         Queue<Integer> q = new LinkedList<>();
+         HashSet<Integer> visited = new HashSet<>();
+         for (int i = 0; i < adj.size(); i++) {
+      if (!visited.contains(i)) {
+        q.add(i);
+      } else {
+        continue;
+      }
+      count++;
+
+      while (!q.isEmpty()) {
+        // 1. remove
+        int rb = q.poll();
+        // 2. continue
+        if (visited.contains(rb)) {
+          continue;
         }
-        // System.out.println("count is: "+ count);
-        return count;
+
+        visited.add(rb);
+
+        for (int nbrs : adj.get(rb)) {
+          if (!visited.contains(nbrs)) {
+            q.add(nbrs);
+          }
         }
+      }
+    }
+    return count;
+    }
 }
-
