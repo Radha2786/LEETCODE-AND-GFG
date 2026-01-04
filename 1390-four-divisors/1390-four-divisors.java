c1a92a37bd2ea9c90540f3ae480a97d0f4df2336
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2026-01-04 23:46:27 +0530

    Time: 43 ms (20.96%), Space: 46.4 MB (51.01%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        // similar approach as sieve-of-eratosthenes
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            int num = nums[i];
+            int count=0;
+            int sum=0;
+            for(int factorial=1; factorial <= Math.sqrt(num) ; factorial++){
+                if(num % factorial == 0){
+                    int other = num/factorial;
+                    if(other==factorial){
+                        count+=1;
+                    }else{
+                        count+=2;
+                        sum+=factorial+other;
+                    }
+                }
+            }
+            if(count==4){
+                result+=sum;
+            }
+        }
+        return result;
+    }
+}
