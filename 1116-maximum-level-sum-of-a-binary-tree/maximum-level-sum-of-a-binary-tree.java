917bb7773682b4bfb41ef77a08923f0a82e8bf8f
Radha Sharma <108953658+Radha2786@users.noreply.github.com>
2026-01-07 00:53:27 +0530

    Time: 10 ms (20.77%) | Memory: 49.2 MB (56.35%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -15,42 +15,43 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-       return MaxLevel(root);
+        return FindMaxLevel(root);
     }
-    public static int MaxLevel(TreeNode root){
+    public static int FindMaxLevel(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        int level=1;
+        int MaxLevel=level;
+        int MaxSum=Integer.MIN_VALUE;
         int sum=0;
-        int max = Integer.MIN_VALUE;
-          Queue<TreeNode> queue = new LinkedList<>();
-          queue.add(root);
-          queue.add(null);
-          int count=1;
-          int maxheight=1;
-           while(!queue.isEmpty()){
-              TreeNode rs=queue.remove();
-              if(rs==null){
-                  if(sum>max){
-                      max=sum;
-                      maxheight=count;
-                  }
-                  count++;
-                  sum=0;
-                  if(queue.isEmpty()){
-                     break; 
-                  }else{
-                      queue.add(null);
-                      continue;
-                  }
-              }else{
-                  sum=sum+rs.val;
-              if(rs.left!=null){
-                  queue.add(rs.left);
-              }
-               if(rs.right!=null){
-                  queue.add(rs.right);
-              }
-              }
-          }
-           return maxheight;
+        while(!queue.isEmpty()){
+            TreeNode curr = queue.remove();
+            if(curr==null){
+                //we found 1 level
+                if(sum>MaxSum){
+                    MaxSum=sum;
+                    MaxLevel=level;
+                }
+                level++;
+                sum=0;
+                if(queue.isEmpty()){
+                    break;
+                }else{
+                    queue.add(null);
+                    continue;
+                    //for adding another level
+                }
+            }else{
+                sum+=curr.val;
+                if(curr.left!=null){
+                    queue.add(curr.left);
+                }
+                if(curr.right!=null){
+                    queue.add(curr.right);
+                }
+            }
+        }
+        return MaxLevel;
     }
-   
 }
